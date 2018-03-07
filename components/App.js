7ba85594bbb96577c8f126003ff3a0ff74661681
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-03-07 06:49:48 +0100

    final task

--- components/App.js
+++ components/App.js
@@ -15,72 +15,60 @@ App = React.createClass({
         this.setState({
             loading: true
         });
-
-        this.getGif(searchingText, function(gif){
-            this.setState({
-                loading: false,
-                gif: gif,
-                searchingText: searchingText
-            });
-        }.bind(this));
+        this.getGif(searchingText)
+            .then((gif) => {
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText
+                });
+            })
+            .catch(error => console.log(error));
     },
 
-function getGif(searchingText, callback) {
-    return new Promise(
-        function (resolve, reject) {
-        const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        const xhr = new XMLHttpRequest();        
+    getGif: function(searchingText) {
 
-        xhr.onload = function() {
-            if (this.status === 200) {
-                let data = JSON.parse(xhr.responseText).data;
-                let gif = {};
-                resolve(this.response);  
-            
-            if(data.length !== 0) {
-                    gif = {
+        return new Promise((resolve, reject) => {
+            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText).data;
+                    var gif = {
                         url: data.fixed_width_downsampled_url,
-                        sourceUrl: data.url,
+                        sourceUrl: data.url
                     };
-                } 
-                callback(gif);
-            } else {
-                reject(new Error(this.statusText));
+                    resolve(gif);
+                } else {
+                    reject(new Error(xhr.status));
+                }
             }
-        };
-        xhr.onerror = function() {
-            reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
-        };
-        xhr.open('GET', url);
-        xhr.send();
-    }
-)},
-
-httpGet(url)
-    .then(response => console.log('Contents: ' + response))
-    .catch(error => console.error('Something went wrong', error));
+            xhr.open('GET', url);
+            xhr.send();
+        });
+    },
 
     render: function(){
 
-        var styles = {
-            margin: '0 auto',
-            textAlign: 'center',
-            width: '90%'
-        };
+            var styles = {
+                margin: '0 auto',
+                textAlign: 'center',
+                width: '90%'
+            };
 
-        return (
-            <div style={styles}>
-                <h1>Gif searcher</h1>
-                <p>Find gif on <a href='http=://giphy'>giphy</a>. Press ENTER for start search more gifs.</p>
-                <Search 
-                    onSearch={this.handleSearch}
-                />
-                <Gif 
-                    loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl}
-                />
-            </div>    
-        )
-    }
-})
+            return (
+                <div style={styles}>
+                    <h1>Gif searcher</h1>
+                    <p>Find gif on <a href='http=://giphy'>giphy</a>. Press ENTER for start search more gifs.</p>
+                    <Search 
+                        onSearch={this.handleSearch}
+                    />
+                    <Gif 
+                        loading={this.state.loading}
+                        url={this.state.gif.url}
+                        sourceUrl={this.state.gif.sourceUrl}
+                    />
+                </div>    
+            )
+        }
+});
